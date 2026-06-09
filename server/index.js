f6186e377b665a4abf5828d94527155fda4c8246
bakshayasai <akshayasai2996@gmail.com>
2026-06-09 13:10:54 +0530

    Debug MongoDB connection issue

--- server/index.js
+++ server/index.js
@@ -8,10 +8,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-console.log(process.env.MONGO_URI);
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected 🎉"))
-  .catch((err) => console.log(err));
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Connected 🎉"))
+.catch((err) => console.log("Mongo Error:", err));
+console.log("MONGO_URI exists?", !!process.env.MONGO_URI);
 let tasks = [
   { id: 1, task: "Learn React" },
   { id: 2, task: "Build MERN App" }
