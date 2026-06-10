254c0007ac8827e7c9c77eff120cbfb3803687c9
bakshayasai <akshayasai2996@gmail.com>
2026-06-10 10:51:03 +0530

    Fix MongoDB connection options error

--- server/index.js
+++ server/index.js
@@ -9,10 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected 🎉"))
 .catch((err) => console.log("Mongo Error:", err));
 console.log("MONGO_URI exists?", !!process.env.MONGO_URI);
